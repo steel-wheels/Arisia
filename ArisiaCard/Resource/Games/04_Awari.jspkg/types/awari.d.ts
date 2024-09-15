fce617a362cc85c7e60316b3894390213fc820fe
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2024-09-15 19:00:34 +0900

    ArisiaLibaray: Update for latest KiwiLibrary

---
+++ ArisiaCard/Resource/Games/04_Awari.jspkg/types/awari.d.ts
@@ -0,0 +1,9 @@
+/// <reference path="ArisiaPlatform.d.ts" />
+declare function tab(space: number): string;
+declare function show_number(number: any): void;
+declare function show_board(): void;
+declare function do_move(): void;
+declare function adjust_board(): void;
+declare let d: number;
+declare function computer_move(): void;
+declare function main(): Promise<void>;
